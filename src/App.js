857cf0ec605d6936c8bdafd0cfdfe60f426abcd6
Wev-Dev-Bibhu <iambibhutidas@gmail.com>
2023-05-17 15:08:06 +0530

    Routing Dashboard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import Dashboard from './components/Dashboard';
 import NavigationBar from './components/NavigationBar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PageNotFound from './components/PageNotFound';
+import { useEffect } from 'react';
 
 function App() {
   return (
     <>
-
       <BrowserRouter>
         <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+        </Routes>
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
         <Routes>
-          <Route path="/dashboard" element={<PageNotFound />} />
           <Route path="/recent" element={<PageNotFound />} />
           <Route path="/settings" element={<PageNotFound />} />
           <Route path="/profile" element={<PageNotFound />} />
